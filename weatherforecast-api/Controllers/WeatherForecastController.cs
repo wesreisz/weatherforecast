864d26b4c64065bdfd0c95a482ab7e68fde9b61e
Wesley Reisz <wes@wesleyreisz.com>
2022-02-08 18:19:36 -0500

    Update WeatherForecastController.cs

--- weatherforecast-api/Controllers/WeatherForecastController.cs
+++ weatherforecast-api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Texas Hot", "Kentucky Crazy"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Texas Hot", "Kentucky Crazy", "Bengals Storming"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
